375dd167dad16367bc1b63d5e0d3a17a32824157
mirnamedeiros <mirnagmedeiros@gmail.com>
2023-06-06 21:55:45 -0300

    Update PassengerTest.java (new tests after using EclEmma)

--- src/airlinereservationsystem/PassengerTest.java
+++ src/airlinereservationsystem/PassengerTest.java
@@ -33,5 +33,41 @@ public class PassengerTest {
 	}
 	
 	
+	//	Casos de Teste (Mostrar Lista de Passageiros)
+	//	Caso 4: Ha pasageiros na lista
+	@Test
+	 public void listagemPassageiros () {
+		// Ler mensagem do console
+		java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();    
+		System.setOut(new java.io.PrintStream(out));    
+
+		Passenger.show_all();
+		boolean comReserva = out.toString().indexOf("No Reservations added yet") !=-1? false: true;
+		assertTrue(comReserva);
+	}
 	
+	//	Casos de Teste (Mostrar lista de reservas para determinado voo)
+	//	Caso 5: Listar reservas de voo existente
+	@Test
+	 public void listagemReservas () {
+		// Ler mensagem do console
+		java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();    
+		System.setOut(new java.io.PrintStream(out));    
+
+		Passenger.show_only_flight_no(2);
+		boolean comReserva = out.toString().indexOf("No Reservations added yet") !=-1? false: true;
+		assertTrue(comReserva);
+	}
+	
+	//	Caso 6: Listar reservas de voo inexistente
+	@Test
+	 public void listagemReservasVooInexistente () {
+		
+		java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();    
+		System.setOut(new java.io.PrintStream(out));    
+
+		Passenger.show_only_flight_no(10);
+		boolean semReserva = out.toString().indexOf("No Reservations added yet") !=-1? true: false;
+		assertTrue(semReserva);
+	}
 }
